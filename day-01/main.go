1c34c67647d7453cd3d3accf82881597a4640ffb
DaKili <mail@kilianwimmer.de>
2023-08-11 14:47:31 +0200

    solve day 1 task 1

--- day-01/main.go
+++ day-01/main.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var inputFile = "input.txt"
 
 func main() {
-	fmt.Println("Day 01 Solution")
+	maxCalorieElf := getMaxCalories()
+	fmt.Printf("Most calories: %v\n", maxCalorieElf)
+}
+
+func getMaxCalories() int {
+	var content, err = os.ReadFile(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var max = 0
+	var current = 0
+	var stringContent = strings.Split(string(content), "\n")
+	for _, line := range stringContent {
+		if line == "" {
+			if current > max {
+				max = current
+			}
+			current = 0
+			continue
+		}
+
+		var i, err = strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		current += i
+	}
+
+	return max
 }
