83143d8a0119876f635a9969b490412e66428b9d
DaKili <mail@kilianwimmer.de>
2023-08-11 15:51:03 +0200

    solve day 1 task 2

--- day-01/main.go
+++ day-01/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"log"
+	"math"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +14,53 @@ var inputFile = "input.txt"
 func main() {
 	maxCalorieElf := getMaxCalories()
 	fmt.Printf("Most calories: %v\n", maxCalorieElf)
+
+	top3CalorieElfs := getTopThreeCalories()
+	fmt.Printf("Top 3 calories: %v %v %v\n", top3CalorieElfs[0], top3CalorieElfs[1], top3CalorieElfs[2])
+	fmt.Printf("Top 3 total calories: %v\n", top3CalorieElfs[0]+top3CalorieElfs[1]+top3CalorieElfs[2])
+}
+
+func getTopThreeCalories() []int {
+	current := 0
+	top3 := []int{0, 0, 0}
+
+	content, err := os.ReadFile(inputFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	stringContent := strings.Split(string(content), "\n")
+	for _, line := range stringContent {
+		if line == "" {
+			indexOfSmallest := indexOfSmallestInt(top3)
+			if current > top3[indexOfSmallest] {
+				top3[indexOfSmallest] = current
+			}
+			current = 0
+			continue
+		}
+
+		i, err := strconv.Atoi(line)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		current += i
+	}
+
+	return top3
+}
+
+func indexOfSmallestInt(array []int) int {
+	idx := 0
+	min := math.MaxInt
+	for i, v := range array {
+		if v < min {
+			min = v
+			idx = i
+		}
+	}
+	return idx
 }
 
 func getMaxCalories() int {
@@ -21,9 +69,9 @@ func getMaxCalories() int {
 		log.Fatal(err)
 	}
 
-	var max = 0
-	var current = 0
-	var stringContent = strings.Split(string(content), "\n")
+	max := 0
+	current := 0
+	stringContent := strings.Split(string(content), "\n")
 	for _, line := range stringContent {
 		if line == "" {
 			if current > max {
@@ -33,7 +81,7 @@ func getMaxCalories() int {
 			continue
 		}
 
-		var i, err = strconv.Atoi(line)
+		i, err := strconv.Atoi(line)
 		if err != nil {
 			log.Fatal(err)
 		}
